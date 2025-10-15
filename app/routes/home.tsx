4e0e595eee0a4760c22a318847881b615d702f18
Thang <thangpham03111999@gmail.com>
2025-10-15 19:47:55 +0700

    commit đầu tiên

---
+++ app/routes/home.tsx
@@ -0,0 +1,19 @@
+import type { Route } from "./+types/home";
+import { Welcome } from "../welcome/welcome";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return <Welcome />;
+}
+
+export namespace Route {
+  export interface MetaArgs {
+    // Add properties as needed for your meta function
+  }
+}
